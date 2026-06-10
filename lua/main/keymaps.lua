b325f4755e7c8a2198a9d9f558722ef92b97d10e
Max Holemans <holemans.max@gmail.com>
2026-06-10 21:20:50 +0200

    Floating diagnostics

--- lua/main/keymaps.lua
+++ lua/main/keymaps.lua
@@ -43,6 +43,8 @@ vim.keymap.set('n', '<leader>e', function()
     local new_config = not vim.diagnostic.config().virtual_lines
     vim.diagnostic.config({virtual_lines=new_config})
 end, { desc = 'Toggle diagnostic virtual_lines' })
+-- Float diagnostic
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float)
 
 
 
